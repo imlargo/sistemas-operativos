ac7855b94f15b7edc50091529a0619108b25ef03
Juan Carlos Largo <134282467+imlargo@users.noreply.github.com>
2024-03-28 15:50:38 +0000

    _

--- playground.c
+++ playground.c
@@ -3,21 +3,9 @@
 #include <unistd.h>
 #include <semaphore.h>
 #include <sys/types.h>
+#include <sys/mman.h>
 
 int main(int argc, char const *argv[]) {
 
-	// Crear semaforo
-	sem_t semaforo;
-	sem_init(&semaforo, 1, 1);
-
-	pid_t pid = fork();
-
-	if (pid == 0) {
-		// Proceso hijo
-	} else {
-		// Proceso padre
-	}
-
-	sem_destroy(&semaforo);
 	return 0;
 }
--- program1.c
+++ program1.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
 
 int main(int argc, char const *argv[]) {
 
-  	int tuberia[2], tuberiaMensajes[2];
+    int tuberia[2], tuberiaMensajes[2];
     pipe(tuberia);
     pipe(tuberiaMensajes);
 
-    // Crear semaforo
-    sem_t semaforo;
-    sem_init(&semaforo, 1, 0);
+    // Crear semáforo
+    sem_t *semaforo;
+    semaforo = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    sem_init(semaforo, 1, 0);
 
     pid_t pid = fork();
 
     if (pid == 0) {
-
         // Proceso hijo
         close(tuberia[1]);
         close(tuberiaMensajes[0]);
-        
+
         char* ruta;
-		read(tuberia[0], &ruta, sizeof(ruta));
+        read(tuberia[0], &ruta, sizeof(ruta));
 
         if (access(ruta, X_OK) != 0) {
             const char *mensaje = "No se encuentra el archivo a ejecutar";
             write(tuberiaMensajes[1], &mensaje, sizeof(mensaje));
+            sem_post(semaforo);
         }
-        
-        if (0) {
+
+        if (1) {
             const char *mensaje = "Proceso 3 no iniciado";
             write(tuberiaMensajes[1], &mensaje, sizeof(mensaje));
+            sem_post(semaforo);
         }
 
     } else {
@@ -46,19 +50,27 @@ int main(int argc, char const *argv[]) {
             printf("Uso: p1 /bin/ls.\n");
             ruta = "/bin/ls";
         } else {
-            // Asignar la ruta del ejecutable a una variable llamada prueba
             ruta = argv[1];
         }
 
-		write(tuberia[1], &ruta, sizeof(ruta));
+        write(tuberia[1], &ruta, sizeof(ruta));
+
+        while (1) {
+            sem_wait(semaforo);
+            char* mensaje;
+            read(tuberiaMensajes[0], &mensaje, sizeof(mensaje));
+            printf("Mensaje recibido: %s\n", mensaje);
+        }
+
     }
 
-	close(tuberia[0]);
-	close(tuberia[1]);
+    close(tuberia[0]);
+    close(tuberia[1]);
 
     close(tuberiaMensajes[0]);
     close(tuberiaMensajes[1]);
 
-    sem_destroy(&semaforo);
+    sem_destroy(semaforo);
+    munmap(semaforo, sizeof(sem_t)); // Liberar el área de memoria compartida
     return 0;
 }
